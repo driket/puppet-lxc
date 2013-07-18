9b2b344713f777ee5566670c2608d09f444c3367
Cedric Villa <cedric.villa@gmail.com>
2013-07-18 19:18:41 +0000

    update lxc

--- manifests/tools/vm_templates.pp
+++ manifests/tools/vm_templates.pp
@@ -1,19 +1,19 @@
 define mv_lxc::tools::vm_templates (
 	$install_directory = $mv_lxc::params::lxc_tools_directory,
-	$provisioning_server,
+	$provisioning_server_fqdn,
+	$provisioning_server_ip,
 ) {
 		
 	require('mv_lxc')
 	require('mv_lxc::packages')
 
 	# puppet
-	file { "${install_directory}/fetch_lxc_vm_templates.sh": 
+	file { "${install_directory}/template-tools.sh": 
   	ensure 	=> file,
     mode		=> "0700",
     owner 	=> 'root',
     group 	=> 'root',
-		content => template("mv_lxc/fetch_lxc_vm_templates.sh.erb"),
+		content => template("mv_lxc/template-tools.sh.erb"),
 		require => File["$install_directory"],
 	}
-
 }
